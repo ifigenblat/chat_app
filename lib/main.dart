ee1eadf4438ea31da01aa71bbe4f3016679a3fd9
ifigenblat <45586168+ifigenblat@users.noreply.github.com>
2025-02-12 10:39:26 -0800

    my first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,22 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NewHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class NewHomePage extends StatefulWidget {
-  const NewHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
 
   final String title;
 
   @override
-  State<NewHomePage> createState() => _NewHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _NewHomePageState extends State<NewHomePage> {
+class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
