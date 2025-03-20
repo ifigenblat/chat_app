71ae9c0a63fce7fe884cfb039f6cdf265bc48b1c
Itsik Figenblat <ifigenblat@itsiks-macbook-pro.local>
2025-03-20 14:52:41 -0700

    catching up to latest

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+   LoginPage({super.key});
+   final _formkey = GlobalKey<FormState>();
 
-  void loginUser(){
-   print('Login successful!');
+  void loginUser() {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
+      print(userNameController.text);
+      print(passwordController.text);
+      print('Login successful!');
+    } else {
+      print('Not successful! ');
+    }
   }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,13 +60,59 @@ class LoginPage extends StatelessWidget {
               //       image: DecorationImage(image: NetworkImage('https://3009709.youcanlearnit.net/Alien_LIL_131338.png')),
               //       color: Colors.blue, borderRadius: BorderRadius.circular(24)),
               // )
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value){
+                        if(value != null && value.isNotEmpty && value.length < 5){
+                          return "Your username should be more than 5 characters";
+                        } else if(value != null && value.isEmpty){
+                          return "Please type your username";
+                        }
+                        return null;
+                      },
+                      controller: userNameController,
+                      decoration: InputDecoration(
+                        hintText: 'Add your username',
+                      hintStyle: TextStyle(color: Colors.blueGrey),
+                      border: OutlineInputBorder()
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: 'Type your Password',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
               ElevatedButton(onPressed: (){
                 loginUser();
                 print('Clicked!');
-              }, child: Text('Click Me!', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300 )),
+              }, child: Text('Login', style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300 )),
                 ),
             //  OutlinedButton(onPressed: (){}, child: FlutterLogo()),
-              GestureDetector(
+             // InkWell(
+               // splashColor: Colors.red,
+            GestureDetector(
+                onDoubleTap: (){
+                  print('double tapped!');
+                },
+                onLongPress: (){
+                  print('on long press!');
+                },
                 onTap: (){
                   //todo: Naviaget to browser
                   print('Link clicked!');
